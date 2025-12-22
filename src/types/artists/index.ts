f9e07ee37146d2a62020f621ac9c5ff27830d2ea
stupilman <creworroad@naver.com>
2025-12-22 15:02:25 +0900

    feat(artist-detail) : 아티스트 상세 타입 정의

--- src/types/artists/index.ts
+++ src/types/artists/index.ts
@@ -5,8 +5,43 @@ export type ApiResponse<T> = {
   data: T;
 };
 
+export type ArtistAlbum = {
+  albumName: string;
+  releaseDate: string;
+  albumType: string;
+  imageUrl: string;
+  spotifyUrl: string;
+};
+
+export type ArtistTopTrack = {
+  trackName: string;
+  spotifyUrl: string;
+};
+
+export type RelatedArtist = {
+  artistName: string;
+  imageUrl: string | null;
+  spotifyArtistId: string;
+};
+
+export interface ArtistDetail {
+  artistName: string;
+  artistGroup: string | null;
+  artistType: "SOLO" | "GROUP";
+  profileImageUrl: string;
+  likeCount: number;
+  totalAlbums: number;
+  popularityRating: number;
+  description: string;
+
+  albums: ArtistAlbum[];
+  topTracks: ArtistTopTrack[];
+  relatedArtists: RelatedArtist[];
+}
+
 export type ArtistListResponse = ApiResponse<ArtistListItem[]>;
 export type LikeArtistResponse = ApiResponse<null>;
+export type ArtistDetailResponse = ApiResponse<ArtistDetail>;
 
 export type ArtistListItem = {
   id: number;
