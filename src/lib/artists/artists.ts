3e121631615881b2f3b68336a16860e3723cd4a6
stupilman <creworroad@naver.com>
2025-12-22 15:13:27 +0900

    feat(artist-detail) : 아티스트 상세 불러오기 함수 정의

--- src/lib/artists/artists.ts
+++ src/lib/artists/artists.ts
@@ -1,5 +1,12 @@
-import { ArtistListItem, ArtistListResponse, LikeArtistResponse } from "@/types/artists";
+import {
+  ArtistDetail,
+  ArtistDetailResponse,
+  ArtistListItem,
+  ArtistListResponse,
+  LikeArtistResponse,
+} from "@/types/artists";
 
+// 아티스트 목록 불러오기
 export async function getArtists(): Promise<ArtistListItem[]> {
   try {
     // fetch (네트워크 단계)
@@ -40,6 +47,7 @@ export async function getArtists(): Promise<ArtistListItem[]> {
   }
 }
 
+// 아티스트 팔로우
 export async function likeArtist(id: number): Promise<void> {
   try {
     // fetch (네트워크 단계)
@@ -65,6 +73,47 @@ export async function likeArtist(id: number): Promise<void> {
     }
 
     // 성공
+  } catch (err) {
+    // 네트워크 에러 (Failed to fetch)
+    if (err instanceof TypeError) {
+      throw new Error("서버에 연결할 수 없습니다. 네트워크 상태를 확인해주세요.");
+    }
+
+    // 내가 던진 Error는 그대로 전달
+    if (err instanceof Error) {
+      throw err;
+    }
+
+    // 정말 예외적인 케이스
+    throw new Error("알 수 없는 오류가 발생했습니다.");
+  }
+}
+
+// 아티스트 상세 불러오기
+export async function getArtistDetail(id: number): Promise<ArtistDetail> {
+  try {
+    // fetch (네트워크 단계)
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/v1/artists/${id}`, {
+      cache: "no-store",
+    });
+
+    // JSON 파싱 단계 (여기서도 에러 날 수 있음)
+    let json: ArtistDetailResponse;
+
+    try {
+      json = (await res.json()) as ArtistDetailResponse;
+    } catch {
+      // JSON 파싱 실패 (빈 응답, HTML 응답 등)
+      throw new Error("서버 응답을 처리할 수 없습니다.");
+    }
+
+    // HTTP / 비즈니스 에러 분기
+    if (!res.ok || json.resultCode !== "OK") {
+      throw new Error(json.msg ?? "아티스트 정보를 불러오는 중 오류가 발생했습니다.");
+    }
+
+    // 성공
+    return json.data;
   } catch (err) {
     // 네트워크 에러 (Failed to fetch)
     if (err instanceof TypeError) {
