a81ef825fa484e51dfbf00aafb66bc4511aa21fe
ivoryShield <sangapark34@naver.com>
2025-12-28 20:39:32 +0900

    refactor(footer) : 푸터 리팩토링, 반응형 작업

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import "@/css/globals.css";
 import Header from "@/components/home/header";
-import Footer from "@/components/common/Footer";
+import Footer from "@/components/home/footer";
 
 import { pretendard } from "@/../public/fonts/local_fonts";
 
