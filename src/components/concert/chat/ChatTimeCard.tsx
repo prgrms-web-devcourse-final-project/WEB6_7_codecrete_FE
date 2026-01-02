828e6b40526f7da9336bf5ddc20fb23b8eb82d84
stupilman <creworroad@naver.com>
2026-01-03 03:25:52 +0900

    feat(chat) : 서버 시간을 활용한 티켓 사이트별 시계 구현

--- src/components/concert/chat/ChatTimeCard.tsx
+++ src/components/concert/chat/ChatTimeCard.tsx
@@ -2,13 +2,34 @@ import ChatAsideClock from "@/components/concert/chat/ChatAsideClock";
 import TicketVendorSelector from "@/components/concert/chat/TicketVendorSelector";
 import { Card } from "@/components/ui/card";
 import { TicketVendor } from "@/types/chat";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { getServerTimeOffset } from "@/lib/api/chat/chat.client";
 
 export default function ChatTimeCard() {
   const [vendor, setVendor] = useState<TicketVendor>("NOL");
+  const [offset, setOffset] = useState<number>(0);
+
+  useEffect(() => {
+    let mounted = true;
+
+    const fetchOffset = async () => {
+      try {
+        const offsetMillis = await getServerTimeOffset(vendor);
+        if (mounted) setOffset(offsetMillis);
+      } catch (e) {
+        console.error(e);
+      }
+    };
+
+    fetchOffset();
+
+    return () => {
+      mounted = false;
+    };
+  }, [vendor]);
   return (
     <Card className={"gap-8 p-7"}>
-      <ChatAsideClock />
+      <ChatAsideClock offsetMillis={offset} />
       <TicketVendorSelector value={vendor} onChange={setVendor} />
     </Card>
   );
