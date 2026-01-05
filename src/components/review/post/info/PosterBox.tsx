d26b83c2ad38a5f0d0d36063b29e7718b881af44
stupilman <creworroad@naver.com>
2026-01-05 21:45:18 +0900

    feat(review) : 후기글 상단 콘서트 카드 부분 구현

--- src/components/review/post/info/PosterBox.tsx
+++ src/components/review/post/info/PosterBox.tsx
@@ -1,7 +1,21 @@
-export default function PosterBox() {
+"use client";
+
+import Image from "next/image";
+import { useState } from "react";
+
+export default function PosterBox({ posterUrl }: { posterUrl: string | null }) {
+  const fallbackImage = "/ConcertPoster.png";
+  const [src, setSrc] = useState(posterUrl || fallbackImage);
+
   return (
     <div className="relative flex h-40 w-32 shrink-0 items-center justify-center rounded-lg bg-gray-300">
-      <span className="text-text-sub text-sm">Concert Poster</span>
+      <Image
+        src={src}
+        alt={`Concert Poster`}
+        fill
+        sizes={"128px"}
+        onError={() => setSrc(fallbackImage)}
+      />
     </div>
   );
 }
