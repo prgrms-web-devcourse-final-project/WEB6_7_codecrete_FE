06c691038ba5197b4d98fc11ad4d83612e03ddae
ivoryShield <sangapark34@naver.com>
2026-02-27 16:05:23 +0900

    feat(mate): 구인수정 반응형, 일부 스타일 수정

--- src/components/concert-mate/edit/MateEditMain.tsx
+++ src/components/concert-mate/edit/MateEditMain.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import ReviewConfirmSection from "@/components/review/write/ReviewConfirmSection";
 import ReviewFooterActions from "@/components/review/write/ReviewFooterActions";
 import { updateMatePost } from "@/lib/api/community/concert-mate/mate.client";
@@ -16,9 +15,7 @@ import MateWriteSection from "@/components/concert-mate/write/MateWriteSection";
 import PreferenceSelectSection from "@/components/concert-mate/write/PreferenceSelectSection";
 import MateTitleSection from "@/components/concert-mate/write/MateTitleSection";
 import SelectedConcert from "@/components/concert-mate/write/SelectedConcert";
-import MateWriteIntro from "@/components/concert-mate/write/MateWriteIntro";
-import { Card } from "@/components/ui/card";
-import { twMerge } from "tailwind-merge";
+import { FieldGroup } from "@/components/ui/field";
 
 export default function MateEditMain({ concertId, postId, postDetail }: MateEditMainProps) {
   const methods = useForm<MatePostWrite>({
@@ -78,10 +75,9 @@ export default function MateEditMain({ concertId, postId, postDetail }: MateEdit
 
   return (
     <FormProvider {...methods}>
-      <section className="bg-bg-main flex justify-center px-15 py-16">
-        <form className={twMerge(`relative mx-auto flex w-full max-w-400 flex-col gap-5 lg:gap-8`)}>
-          <Card className="gap-8 p-12">
-            <MateWriteIntro />
+      <section className="bg-bg-main space-y-5 px-5 py-6 lg:px-15 lg:py-16">
+        <form className="border-border relative mx-auto flex w-full max-w-400 flex-col gap-5 rounded-lg border p-5 lg:gap-8 lg:p-20">
+          <FieldGroup>
             {/* 콘서트 선택 또는 검색*/}
             {/* TODO : 컴포넌트화 (PlannerCreate.tsx)*/}
             <SelectedConcert initialConcertId={concertId} />
@@ -104,7 +100,7 @@ export default function MateEditMain({ concertId, postId, postDetail }: MateEdit
               isDisabled={!isConfirmed}
               buttonText={"구인글 수정"}
             />
-          </Card>
+          </FieldGroup>
         </form>
       </section>
     </FormProvider>
