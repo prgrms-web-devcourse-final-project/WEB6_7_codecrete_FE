92bdf32ddb63422c3cee1c00dbc81c6cf81cd669
ivoryShield <sangapark34@naver.com>
2026-04-02 18:00:30 +0900

    perf(logo): 기존 svg형식의 로고 파일을 webp로 변경하여 적용

--- src/components/home/header/Logo.tsx
+++ src/components/home/header/Logo.tsx
@@ -5,12 +5,13 @@ export default function Logo() {
   return (
     <Link href="/home" className="shrink-0 dark:brightness-0 dark:invert">
       <Image
-        src="/logo_b.svg"
+        src="/logo_b.webp"
         alt="NCB Ticket"
         width={329}
         height={64}
         className="h-8 w-auto lg:h-12"
         priority
+        sizes="(min-width: 1024px) 329px, 161px"
         fetchPriority="high"
       />
     </Link>
